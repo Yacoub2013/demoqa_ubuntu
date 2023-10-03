80ffa72f80a3b308d6e798b57bba7055ca5a7092
Mustafa <mustafa.yacoub2013@yandex.ru>
2023-10-03 16:57:08 +0300

    2-commit

--- components/components.py
+++ components/components.py
@@ -1,6 +1,7 @@
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import logging
 
 
 class WebElement:
@@ -80,3 +81,4 @@ class WebElement:
 
     def check_css(self, style, value=''):
         return self.find_element().value_of_css_property(style) == value
+
